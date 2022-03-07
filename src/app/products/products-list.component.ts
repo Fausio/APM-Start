88a051447ae8e07cfd88ccb437573149b73b5bc8
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-07 15:11:33 +0200

    ng for and ng if

--- src/app/products/products-list.component.ts
+++ src/app/products/products-list.component.ts
@@ -5,6 +5,35 @@ import { Component } from "@angular/core";
     templateUrl: './products-list.component.html'
 })
 
-export class ProductListComponent{
-pageTitle: string = "Product List 😁";
+export class ProductListComponent {
+    pageTitle: string = "Product List 😁";
+
+    producList: any[] = [
+        {
+
+            "ProductId": 2,
+            "ProductName": "Garden car",
+            "ProductCode": "GTN-SDW",
+            "ReleaseDate": "March 18, 2021",
+            "Description": "15 gallon capacity",
+            "Price": 34.5,
+            "StarRating": 4.2,
+            "ImageUrl": "Assets/images/carden_cart.png",
+            "Available": true
+        }
+        ,
+        {
+
+            "ProductId": 4,
+            "ProductName": "Hummer",
+            "ProductCode": "SMK-KUR",
+            "ReleaseDate": "May 20, 2021",
+            "Description": "15 gallon capacity",
+            "Price": 56.5,
+            "StarRating": 3.2,
+            "ImageUrl": "Assets/images/hummer.png",
+            "Available": false
+        }
+    ];
+
 }
