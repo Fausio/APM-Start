2e08b0cd6346ac2786fa9d45804f0a212ad021a0
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-21 10:46:16 +0200

    convertToSpacespipe

---
+++ src/app/shared/pipes/convert-to-spaces.pipe.ts
@@ -0,0 +1,12 @@
+import { PipeTransform } from "@angular/core";
+
+
+export class convertToSpacespipe implements PipeTransform{
+   
+   
+    transform(value: string, characters: string) : string {
+        
+        throw new Error("Method not implemented.");
+    }
+
+}
