6efa85961fd2e297dea052ce5a29a621c3253c30
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-24 08:14:09 +0200

    shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products//products-list/products-list.component';
-import { convertToSpacespipe } from './shared/pipes/convert-to-spaces.pipe';
-import { StarComponent } from './shared/components/star/star.component';
+
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundPathComponent } from './Error/not-found-path/not-found-path.component';
 import { NavbarComponent } from './shared/nav-bar/navbar/navbar.component';
 import { ProductDetailGuard } from './guard/product/product-detail-guard/product-detail-guard';
+import { ProductModule } from './modules/product/product.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 
 
 @NgModule({
     declarations: [AppComponent,
-        StarComponent,
-        ProductListComponent,
-        convertToSpacespipe,
-        ProductDetailComponent,
+        
+        
         WelcomeComponent,
         NotFoundPathComponent,
         NavbarComponent],
@@ -33,11 +32,11 @@ import { ProductDetailGuard } from './guard/product/product-detail-guard/product
 
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: 'welcome', component: WelcomeComponent },
-            { path: 'products', component: ProductListComponent },
-            { path: 'product/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
-
+           
             { path: '**', component: NotFoundPathComponent }
-        ])
+        ]),
+        ProductModule,
+        SharedModule
 
     ],
 
---
+++ src/app/modules/product/product.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core'; 
+import { ProductDetailComponent } from 'src/app/products/product-detail/product-detail.component';
+import { ProductListComponent } from 'src/app/products/products-list/products-list.component';
+import { convertToSpacespipe } from 'src/app/shared/pipes/convert-to-spaces.pipe'; 
+ 
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from 'src/app/guard/product/product-detail-guard/product-detail-guard';
+import { StarComponent } from 'src/app/shared/components/star/star.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [ProductDetailComponent,
+    ProductListComponent,
+    convertToSpacespipe,
+                       ],
+
+  imports: [  
+    SharedModule,
+    RouterModule.forChild([
+
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+
+    ])
+  ]
+
+})
+export class ProductModule { }
